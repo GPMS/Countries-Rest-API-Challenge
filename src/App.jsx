24e2e1ef700916c6376d83465b5c6f1333560349
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-07-18 14:23:20 -0300

    Keep header content centered

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,23 @@ export default function App() {
     return (
         <BrowserRouter>
             <div className="flex min-h-screen flex-col bg-gray-100 text-gray-950 dark:bg-dark-bg-back dark:text-white">
-                <header className="flex items-center justify-between border-b-2 bg-white px-4 py-5 drop-shadow dark:border-black dark:bg-dark-bg-front">
-                    <h1 className="text-2xl font-extrabold">Where in the world?</h1>
-                    <button className="flex items-center gap-2" onClick={toggleDarkMode}>
-                        {(darkMode && (
-                            <>
-                                <MdDarkMode aria-label="dark mode" />
-                                Dark mode
-                            </>
-                        )) || (
-                            <>
-                                <MdOutlineDarkMode aria-label="light mode" />
-                                Light mode
-                            </>
-                        )}
-                    </button>
+                <header className="items-center border-b-2 bg-white px-4 py-5 drop-shadow dark:border-black dark:bg-dark-bg-front">
+                    <div className="container mx-auto flex justify-between">
+                        <h1 className="text-2xl font-extrabold">Where in the world?</h1>
+                        <button className="flex items-center gap-2" onClick={toggleDarkMode}>
+                            {(darkMode && (
+                                <>
+                                    <MdDarkMode aria-label="dark mode" />
+                                    Dark mode
+                                </>
+                            )) || (
+                                <>
+                                    <MdOutlineDarkMode aria-label="light mode" />
+                                    Light mode
+                                </>
+                            )}
+                        </button>
+                    </div>
                 </header>
                 <Routes>
                     <Route path="/">
